ba582002b7757c3a72adb50dcd82deee2ca2a556
Kokhanevich <nike.k1995@gmail.com>
2019-11-05 19:17:06 +0200

    added entities

---
+++ src/main/java/boot/entity/FaceValue.java
@@ -0,0 +1,47 @@
+package boot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "face_values")
+public class FaceValue {
+    @Id
+    @Column(name = "face_value_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long quantity;
+
+    public FaceValue(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+}
